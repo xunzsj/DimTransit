09dbe8c94aecb68e3e8a34d2583dd65c6c3201e4
世纪 赵 <1012430504@qq.com>
2025-12-26 10:54:34 +0800

    初始化项目

---
+++ src/mainwindow.cpp
@@ -0,0 +1,16 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("尺寸测量报告集成软件");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
---
+++ src/src.pri
@@ -0,0 +1,9 @@
+FORMS += \
+    $$PWD/mainwindow.ui
+
+HEADERS += \
+    $$PWD/mainwindow.h
+
+SOURCES += \
+    $$PWD/main.cpp \
+    $$PWD/mainwindow.cpp
